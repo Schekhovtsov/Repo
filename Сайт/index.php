e1b61ee2f9613d96069ae75d53075a2ee590453f
Serge Schekhovtsov <schekhovtsov@gmail.com>
2013-05-29 15:22:19 +0400

    Обновление

---
+++ Сайт/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML> 
+<html>
+<head>
+<HEAD>
+	<title> НАШ САЙТ </title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+
+<!-- тело -->
+
+</body>
+
+<footer>
+</footer>
+</html>
